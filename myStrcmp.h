2a13cb3ee0b0f0bb39cc833ae1015a842d258e2b
Soojin Choi <schoi4@stuy.edu>
2018-09-30 18:47:52 -0400

    completed strcmp and strcpy

--- myStrcmp.h
+++ myStrcmp.h
@@ -1,24 +1,65 @@
 
 
-int strcmp(char *s1, char *s2 ){
+int myStrcmp(char *s1, char *s2 ){
   int retVal = 0;
   if (*s1 || *s2){
     if (*s1 < *s2){
-      printf ("1");
+      //printf ("1");
       retVal -= 1;
       return retVal;
     }
     else if (*s1 > *s2){
-      printf ("2");
+      //printf ("2");
       retVal += 1;
       return retVal;
     }
     else{
-      printf ("3");
+      //printf ("3");
       s1 += 1;
       s2 += 1;
-      return strcmp(s1, s2);
+      return myStrcmp(s1, s2);
+    }
+  }
+  printf("answer");
+  return retVal;
+}
+
+int myStrncmp(char *s1, char *s2, int n){
+  int retVal = 0;
+  while (n){
+    if (*s1 || *s2){
+      if (*s1 < *s2){
+	//printf ("1");
+	retVal -= 1;
+	return retVal;
+      }
+      else if (*s1 > *s2){
+	//printf ("2");
+	retVal += 1;
+	return retVal;
+      }
+      else{
+	n--;
+	s1+= 1;
+	s2+= 1;
+	return myStrncmp(s1, s2, n);
+      }
+    }
+    else{
+      break;
     }
   }
   return retVal;
+}
+
+
+char * myStrcpy(char *dest, char*source){
+  char *temp = dest;
+  while (*source){
+    *temp = *source;
+    source += 1;
+    temp += 1;
+  }
+  *temp = *source;
+  return dest;
 }
--- strDemo.c
+++ strDemo.c
@@ -1,36 +1,44 @@
 #include <stdio.h>
 #include "myStrcmp.h"
+#include <string.h>
 
 int main(){
 	
-	char pvar1[] = "acde";
-	char pvar2[] = "b";
-	char pvar3[] = "acde";
-	char *pa = "abd";
+  char pvar1[] = "a";
+  char pvar2[] = "b";
+  char pvar3[] = "acde";
+  char *pa = "abd";
 	
-	printf("The use of strcmp and strncmp: \n");
+  printf("The use of strcmp and strncmp: \n");
 	
-	printf("\nUsing strcmp to compare:\n\n");
+  printf("\nUsing strcmp to compare:\n\n");
 	
-	//strcmp:
-	printf("Comparing %s and %s : \n\t", pvar1, pvar2);
-	printf("%d ... -1 Means that var1 was less than var 2 \n", strcmp(pvar1, pvar2));
+  //strcmp:
+  printf("Comparing %s and %s : \n\t", pvar1, pvar2);
+  printf("%d ... %d Means that var1 was less than var 2 \n", myStrcmp(pvar1, pvar2), strcmp(pvar1, pvar2));
 	
-	printf("Comparing %s and %s : \n\t", pvar2, pvar1);
-	printf("%d ... 1 Means that var1 was greater than var 2 \n", strcmp(pvar2, pvar1));
+  printf("Comparing %s and %s : \n\t", pvar2, pvar1);
+  printf("%d ... %d Means that var1 was greater than var 2 \n",myStrcmp(pvar2, pvar1) , strcmp(pvar2, pvar1));
 	
-	printf("Comparing %s and %s : \n\t", pvar1, pvar3);
-	printf("%d ... 0 Even though the first character is the same, the rest do not match causing tht inbalance.\n ----------------------------\n", strcmp(pvar1, pvar3));
+  printf("Comparing %s and %s : \n\t", pvar1, pvar3);
+  printf("%d ... %d Even though the first character is the same, the rest do not match causing tht inbalance.\n ----------------------------\n", myStrcmp(pvar1, pvar3), strcmp(pvar1, pvar3));
 	
-	/*
-	printf("Using strncmp to compare:\n\n");
+  
+    printf("Using strncmp to compare:\n\n");
 	
-	printf("Comparing %s and %s up to %d: \n\t", pvar1, pvar3, 1);
-	printf("%d... only comparing [0,1)\n", strncmp(pvar1, pvar3, 1));
-	
-	printf("Comparing %s and %s up to %d: \n\t", pvar1, pvar3, 4);
-	printf("%d... no error\n", strncmp(pvar1, pvar3, 4));
-	
-	*/
-	return 0;
+    printf("Comparing %s and %s up to %d: \n\t", pvar1, pvar3, 1);
+    printf("%d...%d only comparing [0,1)\n", myStrncmp(pvar1, pvar3, 1) , strncmp(pvar1, pvar3, 1));	
+    printf("Comparing %s and %s up to %d: \n\t", pvar1, pvar3, 4);
+    printf("%d... %d no error\n", myStrncmp(pvar1, pvar2, 4), strncmp(pvar1, pvar3, 4));
+
+  char val1[20] = "hello";
+  char val2[] = "hi";
+  char val3[20] = "hello";
+  char val4[] = "hi";
+  
+  
+  printf ("%s \n", myStrcpy(val1, val2));
+  printf ("%s \n", strcpy(val3, val4));
+    
+  return 0;
 }
