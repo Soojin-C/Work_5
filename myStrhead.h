168a14f3dbdef4c11b224cce5c5a4bb0d66e000e
Soojin Choi <schoi4@stuy.edu>
2018-10-01 02:05:25 -0400

    added make

---
+++ myStrhead.h
@@ -0,0 +1,7 @@
+int myStrlen(char* s);
+int myStrcmp(char* s1, char* s2);
+int myStrncmp(char *s1, char*s2, int n);
+char* myStrcpy(char *dest, char*source);
+char* myStrncpy(char *dest, char *source, int n);
+char* myStrcat(char *dest, char *source);
+char* myStrchr(char *s, char c);
